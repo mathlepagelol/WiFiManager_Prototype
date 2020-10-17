e34b70309a2dc84a80250d59445be5c25c0303dc
Mathieu Lepage <mat_182_42@hotmail.com>
2020-10-16 20:36:40 -0400

    ajout de commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
+// NOM DU PROGRAMME : WiFiManager_PROTOTYPE
+// REALISATION INFORMATIQUE : Mathieu Lepage
+// DATE DE CREATION : 2020/10/16
+// OBJET : Programme test permettant d'ajouter un ESP32 en Acces Point (AP) pour ensuite le connecter à un 
+// périphériques réseau sans fil différent (SSID) à partir du portail de connexion WiFi-Manager
+
 #include <Arduino.h>
 #include <WiFiManager.h>
 
+// Les information de login au point d'accès
 const char* ssid = "ESP32AP";
-const char* password = "devkit1234";
-//"EFC5F27D674F";
+const char* password = "devkit1234"; //"EFC5F27D674F";
 
+// Instanciation d'un object WiFiManager
 WiFiManager wm;
 
 void setup() {
@@ -13,6 +20,7 @@ void setup() {
   delay(1000);
   Serial.println("\n");
 
+  // Fonction qui permet de se connecter au WiFi automatiquement lorsqu'une adresse locale est sauvegardée en mémoire
   if(!wm.autoConnect(ssid, password))
     Serial.println("Erreur de connexion.");
   else
@@ -23,6 +31,9 @@ void loop() {
   // if(touchRead(T0) < 50)
   // {
   //   Serial.println("Suppression des reglages et redemarrage...");
+  //   // Fonction qui permet de réinitialiser (Factory Reset) les adresses enregistrées du WiFiManager dans le ESP32
   //   wm.resetSettings();
+  //   // Fonction qui permet de reboot le microcontrolleur
   //   ESP.restart();
-  }
+  // }
+}
